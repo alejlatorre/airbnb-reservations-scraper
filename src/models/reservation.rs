720147b6fa2c96d228e39768d4568eeefe9d3b39
alejlatorre <alej669@gmail.com>
2024-04-20 01:29:59 -0500

    feat: add vector to dataframe process

--- src/models/reservation.rs
+++ src/models/reservation.rs
@@ -1,3 +1,6 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
 pub struct Reservation {
     pub confirmation_code: String,
     pub status: String,
@@ -12,4 +15,21 @@ pub struct Reservation {
     pub booked_date: String,
     pub listing_name: String,
     pub earnings: String,
+}
+
+#[derive(Debug, Default)]
+pub struct ReservationTable {
+    pub confirmation_code: Vec<String>,
+    pub status: Vec<String>,
+    pub guest_user_full_name: Vec<String>,
+    pub guest_user_phone: Vec<String>,
+    pub guest_details_number_of_adults: Vec<f64>,
+    pub guest_details_number_of_children: Vec<f64>,
+    pub guest_details_number_of_infants: Vec<f64>,
+    pub start_date: Vec<String>,
+    pub end_date: Vec<String>,
+    pub nights: Vec<f64>,
+    pub booked_date: Vec<String>,
+    pub listing_name: Vec<String>,
+    pub earnings: Vec<String>,
 }
